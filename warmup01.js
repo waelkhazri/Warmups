b2c9a2cea69a861779caf2dfd89ea837b0fdac68
Farouk Mestiri <117433957+Mestiri77@users.noreply.github.com>
2024-04-25 10:07:33 +0100

    warmup01.js

---
+++ warmup01.js
@@ -0,0 +1,52 @@
+// 1-using + operator create a function 
+//that combine your partner first and last name and return it.*//
+
+// function declaration
+function MyPartnerName(firstname, lastname) {
+	// return firstname+" "+lastname  
+}
+
+
+function MultipleOf13orNot() {
+
+}
+
+
+
+
+
+// 4-create a function that takes a age as a parametre and return that age in seconds.*//
+
+function AgeInSeconds(age) {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
