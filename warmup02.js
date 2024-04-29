3da5f9345a109a0f7b7c96db7e7112a43ffeed8d
Farouk Mestiri <117433957+Mestiri77@users.noreply.github.com>
2024-04-29 09:00:08 +0100

    Create warmup02.js

---
+++ warmup02.js
@@ -0,0 +1,54 @@
+/**
+ * 1-write a function that returns the product of any number by 3.
+ */
+function productOf3(n){
+  
+
+}
+
+/**
+ * 2-complete the following function multiply to return the result of multiplying x and y
+ */
+function multiply(x,y){
+  
+
+}
+
+
+/**
+ * 3-complete the following function that Convert Fahrenheit to Celsius if the module for converting it
+ */
+function fahrenheitToCelsius(fahrenheit){
+}
+
+
+/**
+ * 4-Define a function named "sqArea", and make it display square area if you knew the length of its side.
+ */
+function sqArea(length) {
+
+}
+ /* 5 - Write a function called scoreToGrade that accepts a number as a parameter and returns a string representing a letter grade corresponding to that score.
+ */
+function scoreToGrade(score){
+}
+/**
+ * 6 - Write a function sameLength that accepts two strings as arguments, and returns true if those strings have the same length, and false otherwise.
+ */
+function sameLength(string1, string2) {      
+}
+/**
+ *! 1 Write a function named helloWorld that:
+ *! takes 1 argument, a language code (e.g. "fr", "es", "en")
+ *! returns "Hello, World" for the given language, for at least 3 languages. It should default to returning English.
+ *! ex: helloWorld('fr') ==> 'Bonjour tout le monde'
+ *!    helloWorld('es') ==> 'Hola, Mundo'
+ *!    helloWorld('') ==> 'Hello, World'
+ *!
+ *! @function helloWorld
+ *! @param {string} str
+ *! @return {string}
+ */
+
+function helloWorld(){
+}
